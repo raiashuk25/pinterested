fdcb70139c4b9ea3946711561539cbae5a00cce3
Ashutosh Rai <raiashutech@gmail.com>
2016-05-23 21:19:59 +0000

    integrated with aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,11 +6,13 @@ Rails.application.configure do
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+
   }
-  }
-     Paperclip::Attachment.default_options[:url] = 'https://ashutech.s3.amazonaws.com'
+           Paperclip::Attachment.default_options[:url] = 'https://ashutech.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+  }
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
