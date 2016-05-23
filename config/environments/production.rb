41c743f25726db493e59e1bf86bc4916eab22dde
Ashutosh Rai <raiashutech@gmail.com>
2016-05-23 20:42:06 +0000

    dhdd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.paperclip_defaults= { 
+    :storage => :s3,
+    :s3_credentials => { 
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -78,12 +85,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {host: 'https://newpins.herokuapp.com/',port: 3000}
-   config.paperclip_defaults= { 
-    :storage => :s3,
-    :s3_credentials => { 
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+ 
  end
