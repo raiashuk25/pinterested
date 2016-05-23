78f3d0819732daf36c660eebe556052f28c7b1c9
Ashutosh Rai <raiashutech@gmail.com>
2016-05-23 07:31:36 +0000

    dhdj

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ config.paperclip_defaults = {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+    Paperclip::Attachment.default_options[:url] = ':ashutech.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
     
   }
