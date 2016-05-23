2f790d9a685931c6412fbcf5f41ce1a9047158fa
Ashutosh Rai <raiashutech@gmail.com>
2016-05-23 20:01:36 +0000

    dhd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {host: 'https://pinterestedrai.herokuapp.com/',port: 3000}
-config.paperclip_defaults = {
-  storage: :s3,
- 
-   }
-}
- 
-
 
 end
